fdb8fba1a1d371c1e7d47ddaa31d7874ec0c9657
itsjoniur <ahmadamoori.dev@gmail.com>
2022-08-23 18:54:39 +0430

    added user struct

--- main.go
+++ main.go
@@ -6,14 +6,27 @@ import (
 	"net/http"
 )
 
+type User struct {
+	Id        uint   `json:"id"`
+	Username  string `json:"username"`
+	Password  string `json:"password"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+	DeletedAt string `json:"deleted_at"`
+}
+
 func main() {
 	port := ":8000"
+	// Handlers
+	http.HandleFunc("/", RootHandler)
 	fmt.Printf("Server is running on %v...\n", port)
 
 	err := http.ListenAndServe(port, logRequest(http.DefaultServeMux))
-	if err != nil {
-		log.Fatal(err)
-	}
+	CheckError(err)
+}
+
+func RootHandler(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("Documentation"))
 }
 
 func logRequest(handler http.Handler) http.Handler {
@@ -21,4 +34,10 @@ func logRequest(handler http.Handler) http.Handler {
 		log.Printf("%s %s %s \n", r.RemoteAddr, r.Method, r.URL)
 		handler.ServeHTTP(w, r)
 	})
+}
+
+func CheckError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
