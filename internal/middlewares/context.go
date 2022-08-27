065b064ed6bc54470388e4b774f46968d485e296
itsjoniur <ahmadamoori.dev@gmail.com>
2022-08-28 00:38:01 +0430

    remove: removed useless log

--- internal/middlewares/context.go
+++ internal/middlewares/context.go
@@ -2,7 +2,6 @@ package middlewares
 
 import (
 	"context"
-	"log"
 	"net/http"
 
 	"github.com/itsjoniur/bitlygo/internal/durable"
@@ -10,7 +9,6 @@ import (
 
 func ContextMiddleware(db *durable.Database) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
-		log.Printf("%#v", db)
 		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 			ctx := context.WithValue(req.Context(), 10, db)
 			next.ServeHTTP(w, req.WithContext(ctx))
