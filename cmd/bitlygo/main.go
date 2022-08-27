3d525217927b4f378df3bdce74d2ce4308a82684
itsjoniur <ahmadamoori.dev@gmail.com>
2022-08-27 17:07:58 +0430

    add: create cmd directory

---
+++ cmd/bitlygo/main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	// initialize configuration
+	// arguments: file, environment
+	// create a database client
+	// arguments: connection info
+	// create logger
+	// serve HTTP
+	return
+}
