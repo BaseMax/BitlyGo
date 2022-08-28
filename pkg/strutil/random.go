ed66296bd2a4cce73f8566722c3e002fec198f18
itsjoniur <ahmadamoori.dev@gmail.com>
2022-08-28 14:57:47 +0430

    add: generate random string

---
+++ pkg/strutil/random.go
@@ -0,0 +1,15 @@
+package strutil
+
+import "math/rand"
+
+var (
+	letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+)
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
