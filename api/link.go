63ab03bb81ba921b263de16095e3f1d46ac85c99
itsjoniur <ahmadamoori.dev@gmail.com>
2022-08-28 15:21:10 +0430

    add: create link from url path

--- api/link.go
+++ api/link.go
@@ -7,6 +7,7 @@ import (
 	"net/url"
 	"strings"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/itsjoniur/bitlygo/internal/models"
 	"github.com/itsjoniur/bitlygo/pkg/strutil"
 )
@@ -83,7 +84,72 @@ func addLinkHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func addLinkByPathHandler(w http.ResponseWriter, req *http.Request) {
-	w.Write([]byte("add link by path"))
+	type Params struct {
+		Name string `json:"name"`
+		Link string `json:"link"`
+	}
+	var err error
+	params := Params{}
+
+	json.NewDecoder(req.Body).Decode(&params)
+	if params.Link == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		resp := map[string]any{
+			"status":  false,
+			"message": "link can not be ampty",
+		}
+		json.NewEncoder(w).Encode(resp)
+		return
+	}
+
+	params.Name = chi.URLParam(req, "name")
+	params.Name, err = strutil.RemoveNonAlphanumerical(params.Name)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		resp := map[string]any{
+			"status":  false,
+			"message": http.StatusText(http.StatusBadRequest),
+		}
+		json.NewEncoder(w).Encode(resp)
+		return
+	}
+
+	if params.Name == "" {
+		params.Name = strutil.RandStringRunes(8)
+	}
+
+	if _, err := url.ParseRequestURI(params.Link); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		resp := map[string]any{
+			"status":  false,
+			"message": "link must be a vaild url",
+		}
+		json.NewEncoder(w).Encode(resp)
+		return
+	}
+
+	link, err := models.CreateLink(req.Context(), 0, params.Name, params.Link)
+	if err != nil && strings.Contains(string(err.Error()), "duplicate key") {
+		w.WriteHeader(http.StatusBadRequest)
+		resp := map[string]any{
+			"status":  false,
+			"message": fmt.Sprintf("link with name `%v` exists", params.Name),
+		}
+		json.NewEncoder(w).Encode(resp)
+		return
+	}
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		resp := map[string]any{
+			"status":  false,
+			"message": http.StatusText(http.StatusInternalServerError),
+		}
+		json.NewEncoder(w).Encode(resp)
+		return
+	}
+
+	json.NewEncoder(w).Encode(link)
 }
 
 func updateLinkHandler(w http.ResponseWriter, req *http.Request) {
