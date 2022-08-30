ddfa2f00aa206401164ca10749fd1cd012e8e8d8
itsjoniur <ahmadamoori.dev@gmail.com>
2022-08-30 23:54:41 +0430

    update migrations

--- migrations/001_init_tables.sql
+++ migrations/001_init_tables.sql
@@ -1,16 +1,40 @@
 -- Write your migrate up statements here
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
 create table links(
-    id serial primary key,
-    owner_id int,
-    name varchar(155) not null unique,
-    link text not null,
-    visits int default 0,
-    created_at timestamp not null default now(),
-    updated_at timestamp not null default now(),
-    expired_at timestamp,
-    deleted_at timestamp
+    id SERIAL PRIMARY KEY,
+    owner_id INTEGER,
+    name VARCHAR(155) NOT NULL UNIQUE,
+    link TEXT NOT NULL,
+    visits INTEGER DEFAULT 0,
+    created_at TIMESTAMP NOT null default NOW(),
+    updated_at TIMESTAMP,
+    expired_at TIMESTAMP,
+    deleted_at TIMESTAMP,
+
+    CONSTRAINT fk_owner_id FOREIGN KEY(owner_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS api_keys(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL UNIQUE,
+    key VARCHAR(255) NOT NULL UNIQUE,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP,
+
+    CONSTRAINT fk_user_id FOREIGN KEY(user_id) REFERENCES users(id)
 );
 ---- create above / drop below ----
-drop table links;
+DROP TABLE links;
+DROP TABLE users;
+DROP TABLE api_keys;
 -- Write your migrate down statements here. If this migration is irreversible
 -- Then delete the separator line above.
